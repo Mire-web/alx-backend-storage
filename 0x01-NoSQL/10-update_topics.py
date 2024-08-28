06ffe86b72c8c3b148045b25de8d8d9d7836773b
mire-web <amahe8664@gmail.com>
2024-08-28 05:21:34 +0100

    dix-code

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,7 +5,7 @@ Change all topics in specified school
 import pymongo
 
 
-def update_topics(mongo.collection, name, topics):
+def update_topics(mongo_collection, name, topics):
     """Update all Topics in a school document"""
-    return mongo.collection.update_many({"name": name},
+    return mongo_collection.update_many({"name": name},
                                         {$set: {"topics": topics}})
