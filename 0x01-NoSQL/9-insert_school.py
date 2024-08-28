fc72a4852307d9468e40eee6c835435acc6f783f
mire-web <amahe8664@gmail.com>
2024-08-28 05:02:56 +0100

    insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,8 +7,4 @@ import pymongo
 
 def insert_school(collection, **kwargs):
     """Merge keywords and insert new object"""
-    new_school_obj = {}
-    for key, value in **kwargs:
-        new_school_obj[key] = value
-    _id = collection.insert_one(new_school_obj)
-    return _id
+    return collection.insert(kwargs)
