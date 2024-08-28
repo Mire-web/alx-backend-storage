0f30d0886ed21260544322de5a94e20e0b2f38f7
mire-web <amahe8664@gmail.com>
2024-08-28 06:18:14 +0100

    logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,4 +8,18 @@ from pymongo import MongoClient
 def nginx_stats():
     with MongoClient() as client:
         db = client.logs
-        get
+        count = db.nginx.count()
+        print("{} logs".format(count))
+        print("Methods:")
+        get_count = db.nginx.count({"method": "GET"})
+        post_count = db.nginx.count({"method": "POST"})
+        put_count = db.nginx.count({"method": "PUT"})
+        patch_count = db.nginx.count({"method": "PATCH"})
+        delete_count = db.nginx.count({"method": "DELETE"})
+        print("\tmethod GET: {}".format(get_count))
+        print("\tmethod POST: {}".format(post_count))
+        print("\tmethod PUT: {}".format(put_count))
+        print("\tmethod PATCH: {}".format(patch_count))
+        print("\tmethod DELETE: {}".format(delete_count))
+        get_status_count = db.nginx.count({"method": "GET", "path": "/status"})
+        print("{} status check".format(get_status_count))
