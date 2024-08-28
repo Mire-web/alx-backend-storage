3c667001252a4df6aa80649bad80af2f85f2bd1c
mire-web <amahe8664@gmail.com>
2024-08-28 06:40:18 +0100

    logged

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,18 +9,23 @@ def nginx_stats():
     """Show the stats about nginx logs stored in mongoDB"""
     with MongoClient() as client:
         db = client.logs
-        count = db.nginx.count()
+        count = db.nginx.count_documents({})
         print("{} logs".format(count))
         print("Methods:")
-        get_count = db.nginx.count({"method": "GET"})
-        post_count = db.nginx.count({"method": "POST"})
-        put_count = db.nginx.count({"method": "PUT"})
-        patch_count = db.nginx.count({"method": "PATCH"})
-        delete_count = db.nginx.count({"method": "DELETE"})
+        get_count = db.nginx.count_documents({"method": "GET"})
+        post_count = db.nginx.count_documents({"method": "POST"})
+        put_count = db.nginx.count_documents({"method": "PUT"})
+        patch_count = db.nginx.count_documents({"method": "PATCH"})
+        delete_count = db.nginx.count_documents({"method": "DELETE"})
         print("\tmethod GET: {}".format(get_count))
         print("\tmethod POST: {}".format(post_count))
         print("\tmethod PUT: {}".format(put_count))
         print("\tmethod PATCH: {}".format(patch_count))
         print("\tmethod DELETE: {}".format(delete_count))
-        get_status_count = db.nginx.count({"method": "GET", "path": "/status"})
+        get_status_count = db.nginx.count_documents({"method": "GET",
+                                                     "path": "/status"})
         print("{} status check".format(get_status_count))
+
+
+if __name__ == '__main__':
+    nginx_stats()
